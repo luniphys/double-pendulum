dd2f12f89090c305a0671e5312469e41e84e81b3
luniphys-win <luniphys@gmail.com>
2026-04-08 20:03:06 +0200

    first ui steps

--- src/double-pendulum/MainWindow.xaml.cs
+++ src/double-pendulum/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace double_pendulum
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
+
     {
+        bool running = false;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                TestButton.Content = "Run";
+                TestTextBlock.Text = "Stopped";
+            }
+            else
+            {
+                TestButton.Content = "Stop";
+                TestTextBlock.Text = "Running";
+            }
+            running = !running;
         }
     }
 }
