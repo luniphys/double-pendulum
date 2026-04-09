3c875aec7ca44d3733d6d0e1d9212d2bf93d6c86
luniphys-win <luniphys@gmail.com>
2026-04-09 18:54:15 +0200

    sliders set with colors

--- src/double-pendulum/view/UserControls/Sliders.xaml.cs
+++ src/double-pendulum/view/UserControls/Sliders.xaml.cs
@@ -1,9 +1,23 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace double_pendulum.view.UserControls
 {
     public partial class Sliders : UserControl
     {
+        public static readonly DependencyProperty SliderColorProperty =
+            DependencyProperty.Register(
+                nameof(SliderColor),
+                typeof(Brush),
+                typeof(Sliders),
+                new PropertyMetadata(Brushes.Yellow));
+
+        public Brush SliderColor
+        {
+            get { return (Brush)GetValue(SliderColorProperty); }
+            set { SetValue(SliderColorProperty, value); }
+        }
         public Sliders()
         {
             InitializeComponent();
