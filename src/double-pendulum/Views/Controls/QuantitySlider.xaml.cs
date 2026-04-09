c03e19c45f1367d229c42ecf8b25b89606cef9a8
luniphys-win <luniphys@gmail.com>
2026-04-09 21:58:13 +0200

    Slidervalues changeable

---
+++ src/double-pendulum/Views/Controls/QuantitySlider.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace double_pendulum.Views.Controls
+{
+    public partial class QuantitySlider : UserControl
+    {
+        public Brush SliderColor
+        {
+            get { return (Brush)GetValue(SliderColorProperty); }
+            set { SetValue(SliderColorProperty, value); }
+        }
+        public static readonly DependencyProperty SliderColorProperty =
+            DependencyProperty.Register(
+                nameof(SliderColor),
+                typeof(Brush),
+                typeof(QuantitySlider),
+                new PropertyMetadata(Brushes.Yellow)); // default value: yellow
+
+
+        public string Quantity
+        {
+            get { return (string)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+        public static readonly DependencyProperty QuantityProperty =
+            DependencyProperty.Register(
+                nameof(Quantity),
+                typeof(string),
+                typeof(QuantitySlider));
+       
+
+        public double QuantityValue
+        {
+            get { return (double)GetValue(QuantityValueProperty); }
+            set { SetValue(QuantityValueProperty, value); }
+        }
+        public static readonly DependencyProperty QuantityValueProperty =
+            DependencyProperty.Register(
+                nameof(QuantityValue),
+                typeof(double),
+                typeof(QuantitySlider));
+
+
+        public double MinSliderValue
+        {
+            get { return (double)GetValue(MinSliderValueProperty); }
+            set { SetValue(MinSliderValueProperty, value); }
+        }
+        public static readonly DependencyProperty MinSliderValueProperty =
+            DependencyProperty.Register(
+                nameof(MinSliderValue),
+                typeof(double),
+                typeof(QuantitySlider));
+
+
+        public double MaxSliderValue
+        {
+            get { return (double)GetValue(MaxSliderValueProperty); }
+            set { SetValue(MaxSliderValueProperty, value); }
+        }
+        public static readonly DependencyProperty MaxSliderValueProperty =
+            DependencyProperty.Register(
+                nameof(MaxSliderValue),
+                typeof(double),
+                typeof(QuantitySlider));
+
+
+
+        public QuantitySlider()
+        {
+            InitializeComponent();
+        }
+    }
+}
